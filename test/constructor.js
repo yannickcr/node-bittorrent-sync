d7a3b4c0c7c91e18d97866148eba96ee24c8ffc3
Yannick Croissant <yannick.croissant@gmail.com>
2013-11-16 00:14:56 +0100

    Add tests for error cases

--- test/constructor.js
+++ test/constructor.js
@@ -1,10 +1,14 @@
 'use strict';
 
 var
+  nock   = require('nock'),
   chai   = require('chai'),
   expect = chai.expect,
   BtSync = require('../lib/bittorrent-sync')
 ;
+
+// Since we do not want to call any API for real we disable all real HTTP requests
+nock.disableNetConnect();
 
 describe('constructor', function() {
 
@@ -48,6 +52,74 @@ describe('constructor', function() {
       expect(btsync.options.username).to.equal('myusername');
       expect(btsync.options.password).to.equal('mypassword');
       expect(btsync.options.timeout).to.equal(5000);
+    });
+
+  });
+
+  describe('with invalid username/password', function() {
+
+    var btsync = new BtSync({
+      username: 'invalid',
+      password: 'invalid'
+    });
+
+    before(function (done) {
+      nock('http://localhost:8888')
+        .get('/api?method=get_version')
+        .reply(401);
+      done();
+    });
+
+    it('must return an authorisation error', function(done) {
+      btsync.getVersion(function(err, result) {
+        expect(err).to.be.instanceof(Error);
+        expect(result).to.equal('');
+        return done();
+      });
+    });
+
+  });
+
+  describe('with invalid hostname or port', function() {
+
+    var btsync = new BtSync({
+      host   : 'invalid',
+      port   : 7357,
+      timeout: 1
+    });
+
+    before(function (done) {
+      nock.enableNetConnect('invalid');
+      done();
+    });
+
+    it('must return a timeout error', function(done) {
+      btsync.getVersion(function(err, result) {
+        expect(err).to.be.instanceof(Error);
+        expect(result).to.equal(undefined);
+        return done();
+      });
+    });
+
+  });
+
+  describe('with invalid response', function() {
+
+    var btsync = new BtSync();
+
+    before(function (done) {
+      nock('http://localhost:8888')
+        .get('/api?method=get_version')
+        .reply(200, 'Hello, I\'m invalid.');
+      done();
+    });
+
+    it('must return a parse error', function(done) {
+      btsync.getVersion(function(err, result) {
+        expect(err).to.be.instanceof(Error);
+        expect(result).to.equal('Hello, I\'m invalid.');
+        return done();
+      });
     });
 
   });
